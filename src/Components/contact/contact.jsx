cf21ca77999a0da57eee028b6f528a276242df20
Anushka kashyap <anushkakashyap50755@gmail.com>
2025-06-28 21:55:54 +0530

    edit contact file

---
+++ src/Components/contact/contact.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './contact.css'
+import Image from "./../../assets/contact.png"
+
+const contact = () => {
+    return (
+        <>
+            <div className='Contact'>
+                <p>Contact US</p>
+                <div className="contact-image">
+                    <img src={Image} alt="" />
+                </div>
+                <div className="Contact-form">
+                    <p>Get in touch</p>
+                    <div className="contact-inp">
+                    <input type="text" placeholder='Your Name'/>
+                    </div>
+                    <div className="contact-inp">
+                    <input type="email" placeholder='Your Email'/>
+                    </div>
+                    <div className="contact-inp">
+                    <input type="password" placeholder='Your Password'/>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default contact

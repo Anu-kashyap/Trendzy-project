06d0a972d37157250102480a2b6f4916f2dc8200
Anushka kashyap <anushkakashyap50755@gmail.com>
2025-06-29 16:15:35 +0530

    backend setup

---
+++ backend/Models/db.js
@@ -0,0 +1,11 @@
+import process from 'process';
+const mongoose = ('mongoose');
+
+const mongo_url = process.env.MONGO_CONN;
+
+mongoose.connect(mongo_url)
+.then(()=>{
+    console.log('MongoDB Connected...');
+}).catch((err)=> {
+    console.log('MongoDB connection Error: ',err);
+})
